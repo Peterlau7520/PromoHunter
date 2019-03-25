cb40b2276b079b5b96a921a773b6e67fe3ed59ce
Alvin <alvinlau2007@gmail.com>
2019-03-26 01:38:47 +0800

    More styling

--- public/javascripts/campaign.js
+++ public/javascripts/campaign.js
@@ -1,3 +1,5 @@
+$("#campaign li").css("background", "linear-gradient(to right, #f7aa5c 0%, #f7aa5c 5%, #fef2e8 5%, #fef2e8 90%)");
+
 var map;
 var pos = navigator.geolocation.getCurrentPosition(initMap);
 function initMap(position) {
